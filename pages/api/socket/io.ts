afff1b2a99c8f30dbed68ab770d9631a61c4a993
Emeka-web-dev <chukwurex042@gmail.com>
2024-06-14 11:51:38 -0700

    socket io9

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -17,6 +17,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const io = new ServerIO(httpServer, {
       path: path,
       addTrailingSlash: false,
+      transports: ["websocket"],
       cors: {
         origin: "https://opes-app-jet.vercel.app",
         methods: ["GET", "POST"],
