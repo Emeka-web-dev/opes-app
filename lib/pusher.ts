fd5f7e5f49289c02aeaef959df055d0c5f7f96ba
Emeka-web-dev <chukwurex042@gmail.com>
2024-06-20 12:55:11 -0700

    pusher3

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -6,7 +6,7 @@ export const pusherServer = new PusherServer({
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: "eu",
-  useTLS: true,
+  // useTLS: true,
 });
 
 /**
